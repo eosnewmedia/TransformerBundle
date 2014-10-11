8de6cc72d68630d18f08205989f4bd8318f29c7c
Philipp Marien <marien@eosnewmedia.de>
2014-10-11 12:20:35 +0200

    #9 only the needed libraries are now in the composer json

--- DependencyInjection/TransformerConfiguration.php
+++ DependencyInjection/TransformerConfiguration.php
@@ -8,8 +8,6 @@
 
 namespace Enm\TransformerBundle\DependencyInjection;
 
-use Enm\TransformerBundle\ConfigurationStructure\StringValidationEnum;
-use Enm\TransformerBundle\ConfigurationStructure\TypeEnum;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
